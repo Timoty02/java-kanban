1960d489544a958d82db9022a9f124ca429c79e0
Tim <timoty02@yandex.ru>
2024-02-16 15:00:17 +0300

    refactor:вынести исключения в отдельный модуль

--- src/tasks/Task.java
+++ src/tasks/Task.java
@@ -35,11 +35,48 @@ public class Task {
 
     @Override
     public String toString() {
-        return "Tasks.Task{" +
-                "name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", id=" + id +
-                ", status='" + status + '\'' +
-                '}';
+        String res = id + "," + TaskType.TASK + "," + name + "," + status + "," + description + ",0";
+        return res;
+    }
+
+    public static Task fromString(String value){
+        String[] values = value.split(",");
+        Task task;
+        switch(values[1]){
+            case "TASK":
+                task = new Task(values[2], values[4]);
+                break;
+            case "EPIC":
+                task = new EpicTask(values[2], values[4]);
+                break;
+            case "SUBTASK":
+                task = new SubTask(values[2], values[4]);
+                ((SubTask) task).setEpicId(Integer.parseInt(values[5]));
+                break;
+            default:
+                task = null;
+                break;
+
+        }
+        TaskStatus res1;
+        switch(values[3]){
+            case "NEW":
+                res1 = TaskStatus.NEW;
+                break;
+            case "DONE":
+                res1 = TaskStatus.DONE;
+                break;
+            case "IN_PROGRESS":
+                res1 = TaskStatus.IN_PROGRESS;
+                break;
+            default:
+                res1 = null;
+                break;
+
+        }
+        assert task != null;
+        task.setStatus(res1);
+        task.setId(Integer.parseInt(values[0]));
+        return task;
     }
 }
